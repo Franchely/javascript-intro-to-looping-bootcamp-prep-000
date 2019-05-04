70d2538940f18fc7ad2594055667b134568c5bca
Franchely Portorreal <franchely811@gmail.com>
2019-05-04 02:18:32 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -15,14 +15,14 @@ function whileLoop(Number) {
 }
 
 
-function doWhileLoop(i) {
+function doWhileLoop(num) {
   function incrementVariable() {
   i = i + 1;
   return i;
 }
  do {
    console.log("I run once regardless.");
- } while (incrementVariable() < i);
+ } while (incrementVariable() < num);
 }
 
 
